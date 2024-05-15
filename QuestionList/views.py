2eb3dedecf8faf8b7b0c187214481c3207a54ea4
SeoRish <seojin0774@naver.com>
2024-05-15 20:39:21 +0900

    update:questionlist api

--- QuestionList/views.py
+++ QuestionList/views.py
@@ -24,7 +24,7 @@ class ChatGPTView(APIView):
             return Response({"error": "분야와 직무 입력은 필수입니다."}, status=status.HTTP_400_BAD_REQUEST)
 
         # OpenAI API 요청을 위한 입력값 수정
-        modified_input = f"{user_input_field}분야의 {user_input_job}직무와 관련된 면접 질문 10가지 리스트업해줘 한국어로"
+        modified_input = f"{user_input_field}분야의 {user_input_job}직무와 관련된 면접 질문 10가지 리스트업해줘 질문 번호 없이 질문 텍스트만 뽑아줘 한국어로"
         try:
             # OpenAI API 호출
             response = requests.post(
