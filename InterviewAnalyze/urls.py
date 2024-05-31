c3d719ced9db7d397d97998a981d3c2bdcea5ee4
SeoRish <seojin0774@naver.com>
2024-05-31 23:08:45 +0900

    이서 voice post code merge done

--- InterviewAnalyze/urls.py
+++ InterviewAnalyze/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ResponseAPIView
+from .views import *
 
 urlpatterns = [
     path('responses/<int:question_list_id>/', ResponseAPIView.as_view(), name='interview_responses'),
     # path('responses/<int:question_list_id>/analyze/', AnalyzeResponseAPIView.as_view(), name='analyze_responses'),
+   
+    path('voice/', VoiceAPIView.as_view(), name='voice'), 
+    path('voice/<int:question_list_id>/', VoiceAPIView.as_view(), name='voice_with_question_list')
 ]
