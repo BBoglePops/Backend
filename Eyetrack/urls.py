08cbe57a6a11a2ee492b3cc49c892fd357fb3879
SeoRish <seojin0774@naver.com>
2024-06-02 02:43:19 +0900

    Resolve merge conflict in db.sqlite3

--- Eyetrack/urls.py
+++ Eyetrack/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import start_gaze_tracking_view, stop_gaze_tracking_view
 
 urlpatterns = [
-    path('start/<int:user_id>/', start_gaze_tracking_view, name='start_gaze_tracking'),
-    path('stop/<int:user_id>/', stop_gaze_tracking_view, name='stop-gaze-tracking'),
+    path('start/<int:user_id>/<int:interview_id>/', start_gaze_tracking_view, name='start_gaze_tracking'),
+    path('stop/<int:user_id>/<int:interview_id>/', stop_gaze_tracking_view, name='stop-gaze-tracking'),
 ]
 
 
