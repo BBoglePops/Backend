6cab51c359bdfb1649ef835078ab37e8b15f073d
SeoRish <seojin0774@naver.com>
2024-08-10 01:19:53 +0900

    plzzz

--- Eyetrack/serializers.py
+++ Eyetrack/serializers.py
@@ -10,12 +10,15 @@ class VideoSerializer(serializers.ModelSerializer):
         fields = ['id', 'file', 'uploaded_at']
 
 # Signed URL 생성을 위한 시리얼라이저
+from rest_framework import serializers
+
 class SignedURLSerializer(serializers.Serializer):
-    file_name = serializers.CharField(max_length=255)  # 업로드할 파일의 이름
-    content_type = serializers.CharField(max_length=50)  # 파일의 MIME 타입
+    file_name = serializers.CharField(max_length=255, required=False)  # 업로드할 파일의 이름, 필수 아님
+    content_type = serializers.CharField(max_length=50, required=False)  # 파일의 MIME 타입, 필수 아님
 
     def validate_content_type(self, value):
-        # MIME 타입 검증 로직 (예제)
-        if value != 'video/webm':
+        # MIME 타입 검증 로직
+        if value and value != 'video/webm':
             raise serializers.ValidationError("Only .webm video types are allowed.")
         return value
+
