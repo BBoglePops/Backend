c8a18b9938d2b47c772bf1730d155fdab4fa3d1b
SeoRish <seojin0774@naver.com>
2024-08-09 16:39:58 +0900

    url update

--- Eyetrack/urls.py
+++ Eyetrack/urls.py
@@ -6,8 +6,8 @@ from django.urls import path
 from .views import start_gaze_tracking_view, stop_gaze_tracking_view, VideoUploadView, SignedURLView
 
 urlpatterns = [
-    path('start/<int:user_id>/<int:interview_id>/<int:question_id>/', start_gaze_tracking_view, name='start_gaze_tracking'),
-    path('stop/<int:user_id>/<int:interview_id>/<int:question_id>/', stop_gaze_tracking_view, name='stop-gaze-tracking'),
+    path('start/<int:user_id>/<int:interview_id>/', start_gaze_tracking_view, name='start_gaze_tracking'),
+    path('stop/<int:user_id>/<int:interview_id>/', stop_gaze_tracking_view, name='stop-gaze-tracking'),
     path('upload/', VideoUploadView.as_view(), name='file-upload'),
    path('generate-signed-url/<int:user_id>/<int:interview_id>/', SignedURLView.as_view(), name='generate-signed-url'),
 ]
